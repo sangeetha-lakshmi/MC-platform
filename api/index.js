6a8c2104bcde564852945d7205cf66b9ca19b6a4
sangeetha-lakshmi <sangeethalachu0@gmail.com>
2026-02-03 15:55:40 +0530

    edited

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
-const app = require("../src/app");
+import express from "express";
 
-module.exports = app;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API working ✅" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found ❌" });
+});
+
+export default app;
