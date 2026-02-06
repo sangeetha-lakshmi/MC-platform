f70e2183d64b2b3956183f0456e21f4b1a3e5292
Rashitha <rashithar021@gmail.com>
2026-02-06 10:04:08 +0530

    customers order completed

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-<<<<<<< HEAD
 const adminController = require("../controller/admin.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
-=======
-const adminController = require("../controller/admin.controller.js");
-const authMiddleware = require("../middlewares/auth.middleware");
-const roleMiddleware = require("../middlewares/role.middleware");
->>>>>>> 575455205a6ee4f66a67e54fc2ec7523fb393172
-// ✅ Check what is imported
+
+// check import
 console.log("Admin Controller Loaded:", adminController);
 
 // Routes
@@ -18,20 +13,11 @@ router.post("/login", adminController.adminLogin);
 router.get("/pending", adminController.getPendingVendors);
 
 router.put("/approve/:id", adminController.approveVendor);
-router.put(
-  "/vendors/:vendorId/decline",
-  authMiddleware,
-  roleMiddleware("admin"),
-  adminController.declineVendor
-);
-router.get(
-  "/vendors/approved",
-  authMiddleware,
-  roleMiddleware("admin"),
-  adminController.getApprovedVendors
-);
-console.log("Admin Controller Loaded:", Object.keys(adminController));
 
-router.put("/change-password",authMiddleware,adminController.changeAdminPassword);
+router.put(
+  "/change-password",
+  authMiddleware,
+  adminController.changeAdminPassword
+);
 
 module.exports = router;
